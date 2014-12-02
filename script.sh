7ef4ad98d019ec9fab13df04ae63b4e899afe8c9
U-Zyn Chua <chua@uzyn.com>
2014-12-02 19:07:29 +0800

    init

--- script.sh
+++ script.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sudo apt-get install -y python-software-properties
-sudo add-apt-repository ppa:rethinkdb/ppa -y
 sudo apt-get update -y
-sudo apt-get install rethinkdb -y
-sudo mkdir -p /etc/rethinkdb/instances.d
-sudo -- sh -c "echo 'bind=all' > /etc/rethinkdb/instances.d/default.conf"
+sudo apt-get install postgresql -y
+
+echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/9.1/main/pg_hba.conf 
+sudo service postgresql restart
+
+for i in $(seq 1 5);
+do
+	export database"$i"="werckerdb"$i
+	export user"$i"="postgres"$i
+	sudo -u postgres psql -c "CREATE USER postgres"$i" WITH PASSWORD 'wercker';"
+	sudo -u postgres psql -c "CREATE DATABASE werckerdb"$i";"
+	sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE werckerdb"$i" TO postgres"$i";"
+done
